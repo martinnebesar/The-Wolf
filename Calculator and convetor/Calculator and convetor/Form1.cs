3bf5f18dc81d9e80b3bf5347384157ba62d98ae8
martinnebesar <56178649+martinnebesar@users.noreply.github.com>
2020-01-24 16:13:47 -0500

    project redesigned and updated

---
+++ Calculator and convetor/Calculator and convetor/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_and_convetor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(textBox1.Text);
+            double num2 = double.Parse(textBox2.Text);
+            double result = 0;
+            if (radioButton1.Checked == true) // addition
+            {
+                result = num1 + num2;
+            }
+             else if (radioButton2.Checked == true) // substraction
+            { 
+                result = num1 - num2;
+            }
+            else if (radioButton3.Checked == true) // multipication
+            {
+                result = num1 * num2;
+            }
+            else if (radioButton4.Checked == true) // division
+            { 
+                result = num1 / num2;
+            }
+            textBox3.Text = result.ToString();
+        }
+
+       
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if
+        }
+    }
+}
