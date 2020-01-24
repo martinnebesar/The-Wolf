1cf06d7a8ba3e65e3e5d362be1988a14fc12c837
martinnebesar <nebesar.martin@gmail.com>
2020-01-24 18:51:22 -0500

    chapter 9 updated

---
+++ Chapter09ProgramClassDesign/Chapter09ProgramClassDesign/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Chapter09ProgramClassDesign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblLeapYearResult = new System.Windows.Forms.TextBox();
+            this.lblEasterResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter year:";
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(77, 14);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(100, 20);
+            this.txtYear.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ca&lculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(102, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "&Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // lblLeapYearResult
+            // 
+            this.lblLeapYearResult.Location = new System.Drawing.Point(12, 93);
+            this.lblLeapYearResult.Name = "lblLeapYearResult";
+            this.lblLeapYearResult.ReadOnly = true;
+            this.lblLeapYearResult.Size = new System.Drawing.Size(165, 20);
+            this.lblLeapYearResult.TabIndex = 4;
+            // 
+            // lblEasterResult
+            // 
+            this.lblEasterResult.Location = new System.Drawing.Point(12, 119);
+            this.lblEasterResult.Name = "lblEasterResult";
+            this.lblEasterResult.ReadOnly = true;
+            this.lblEasterResult.Size = new System.Drawing.Size(165, 20);
+            this.lblEasterResult.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(189, 155);
+            this.Controls.Add(this.lblEasterResult);
+            this.Controls.Add(this.lblLeapYearResult);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Class Design";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox lblLeapYearResult;
+        private System.Windows.Forms.TextBox lblEasterResult;
+    }
+}
+
---
+++ Chapter09ProgramClassDesign/Chapter09ProgramClassDesign/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter09ProgramClassDesign
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+             
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            bool flag;
+            int year;
+            int leap;
+            clsDates myDate = new clsDates();
+
+            // Convert valide integer
+            flag = int.TryParse(txtYear.Text, out year);
+            if (flag == false)
+            {
+                MessageBox.Show("Digit characters only in YYYY format.", "Input Error");
+                txtYear.Focus();
+                return;
+            }
+            leap = myDate.getLeapYear(year);
+            lblLeapYearResult.Text = year.ToString() + " is " + ((leap == 1) ? "" : "not ") + "a leap year";
+            lblEasterResult.Text = myDate.getEaster(year);
+        }
+    }
+}
