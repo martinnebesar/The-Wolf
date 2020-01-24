2506ea26159c66f29468af566a3de97b3a1dc70a
martinnebesar <nebesar.martin@gmail.com>
2020-01-23 19:31:50 -0500

    chapter 8 uploaded

---
+++ Chapter08ProgramLetterCount/Chapter08ProgramLetterCount/frmMain.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Chapter08ProgramLetterCount
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.Location = new System.Drawing.Point(12, 283);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(284, 186);
+            this.lstOutput.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(264, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Count";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Letter";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(224, 209);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 209);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 9;
+            this.btnCalculate.Text = "Ca&lculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 39);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(287, 153);
+            this.txtInput.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Enter text:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 478);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.Text = "frmMain";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstOutput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        protected internal System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Chapter08ProgramLetterCount/Chapter08ProgramLetterCount/frmMain.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter08ProgramLetterCount
+{
+    public partial class frmMain : Form
+    {
+        private const int MAXLETTERS = 26;          // Symbolic constant
+        private const int MAXCHARS = MAXLETTERS - 1;
+        private const int LETTERA = 65;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            char oneLetter;
+            int index;
+            int i;
+            int length;
+            int[] count = new int[MAXLETTERS];
+            string input;
+            string buff;
+
+            length = txtInput.Text.Length;
+            if (length == 0)    // Anything to count??
+            {
+                MessageBox.Show("You need to enter some text.", "Missing Input");
+                txtInput.Focus();
+                return;
+            }
+            input = txtInput.Text;
+            input = input.ToUpper();
+
+            for (i = 0; i < input.Length; i++)      // Examine all letters.
+            {
+                oneLetter = input[i];               // Get a character
+                index = oneLetter - LETTERA;        // Make into an index
+                if (index < 0 || index > MAXCHARS)  // A leterr??
+                    continue;                       // Nope.
+                count[index]++;                     // Yep.
+            }
+            for (i = 0; i < MAXLETTERS; i++)
+            {
+                buff = string.Format("{0, 4} {1, 20} [{2}]", (char) (i + LETTERA), " ", count[i]);
+                lstOutput.Items.Add(buff);
+            }
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
