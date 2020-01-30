14cc66880491de154ea9b90416701e8388b83cf6
martinnebesar <nebesar.martin@gmail.com>
2020-01-30 17:41:38 -0500

    Exam practice 1 updated

---
+++ Exam practice 1/Exam practice 1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_practice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Uber");
+            // Test Cases
+            int[] arrInput = { 1, 2, 3 };
+            // Output Array
+            int[] arrOutput = new int[arrInput.Length];
+
+            int result = 1; // initializing to 1 because 0 * X = 0
+
+            // for i
+            for (int i = 0; i < arrInput.Length; i++)
+            {
+                // so if Length = 3
+                // I will do i = 0, i = 1, i = 2 passes
+                
+
+                // for j
+                for (int j = 0; j < arrInput.Length; j++)
+                {
+
+                    // print to demonstrate
+                    if (i != j)
+                    {
+                        result *= arrInput[j];
+                    }
+                    //  i = 0
+                    
+                }
+                // show Output
+                arrOutput[i] = result;
+                result = 1;
+
+            }
+            for (int x = 0; x < arrOutput.Length; x++)
+            {
+                Console.WriteLine(arrOutput[x]);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
